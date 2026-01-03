861c0b36e09051805e6fcfacde10a3306ded0f9c
Jagadeeshreddy14 <jagadeeshreddy14@gmail.com>
2026-01-03 13:57:21 +0530

    feat: Initialize Express server with authentication, user, project, and Stripe webhook handling.

--- server/server.ts
+++ server/server.ts
@@ -13,17 +13,11 @@ const app = express();
 const port = 3000;
 
 const corsOption: CorsOptions = {
-    origin: process.env.TRUSTED_ORIGINS
-        ? process.env.TRUSTED_ORIGINS.split(",")
-        : [
-              "http://localhost:5173",
-              "https://ai-website-builder-tzsv.vercel.app",
-          ],
+    origin: process.env.TRUSTED_ORIGINS?.split(",") || [],
     credentials: true,
 };
 
 app.use(cors(corsOption));
-app.options("*", cors(corsOption));
 app.post(
     "/api/stripe",
     express.raw({ type: "application/json" }),
